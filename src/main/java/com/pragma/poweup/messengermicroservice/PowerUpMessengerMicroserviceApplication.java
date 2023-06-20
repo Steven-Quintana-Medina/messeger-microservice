8d2e8d0a2c0518600425224f433f207c9036d19d
steven <quintanasteven57@gmail.com>
2023-06-19 23:59:14 -0500

    initial commit 🚀

---
+++ src/main/java/com/pragma/poweup/messengermicroservice/PowerUpMessengerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.poweup.messengermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerUpMessengerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerUpMessengerMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/poweup/messengermicroservice/PowerUpMessengerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.poweup.messengermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerUpMessengerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
